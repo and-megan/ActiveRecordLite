71afb7c36569cdc99f61fe5cf04fe78b113fe950
Megan Anderson <megroseanderson@gmail.com>
2016-05-15 22:28:03 -0700

    finish associations

--- lib/04_associatable2.rb
+++ lib/04_associatable2.rb
@@ -1,10 +1,32 @@
 require_relative '03_associatable'
 
-# Phase IV
-module Associatable
-  # Remember to go back to 04_associatable to write ::assoc_options
 
-  def has_one_through(name, through_name, source_name)
-    # ...
+module Associatable
+
+  def has_many_through(name, through, source)
+    define_method(name) do
+      association = self.send(through)
+      unless association.kind_of?(Array)
+        association.to_a
+      end
+
+      has_many_through = []
+
+      association.each do |object|
+        has_many_through =
+            has_many_through.concat(object.send(source))
+      end
+
+      has_many_through
+    end
   end
+
+  def has_one_through(name, through, source)
+
+    define_method(name) do
+      through_class = self.send(through)
+      through_class.send(source)
+    end
+  end
+
 end

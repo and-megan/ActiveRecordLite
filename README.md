a8bf21006810d79fd3334650d072fe9d2500a91c
Megan Anderson <megroseanderson@gmail.com>
2016-05-16 16:06:39 -0700

    demo.rb

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 #Active Record Lite
 
 ##Summary
-ActiveRecordLite was built to explore how database changes occur using Ruby on Rails' ActiveRecord, particularly how ActiveRecord translates associations and queries into SQL automatically. This project further demonstrates meta-programming knowledge.
+ActiveRecordLite was built to explore how database changes occur using Ruby on Rails' ActiveRecord, particularly how ActiveRecord translates associations and queries into SQL automatically. This project further demonstrates meta-programming knowledge. This project does not include every feature of ActiveRecord, but contains many popular methods such as the find, where, has_many, belongs_to, has_one_through, and has_many_through methods of ActiveRecord::Base.
 
-ActiveRecordLite contains hand-rolled versions of the find, where, has_many, belongs_to, has_one_through, and has_many_through methods of ActiveRecord::Base.
+ActiveRecordLite uses SQLite for its database engine.
---
+++ demo.rb
@@ -0,0 +1,64 @@
+require_relative 'lib/active_record_lite'
+
+ENV['DEBUG'] = 'true'
+
+DBConnection.open('db/bikes.sqlite3')
+
+class Bike < SQLObject
+  my_attr_accessor :id, :name, :owner_id
+
+  belongs_to :human, foreign_key: :owner_id
+  has_one_through :lock, :human, :house
+end
+
+
+class Human < SQLObject
+  set_table_name 'humans'
+  my_attr_accessor :id, :fname, :lname, :house_id
+
+  has_many :bikes, foreign_key: :owner_id
+  belongs_to :house
+end
+
+
+class House < SQLObject
+  my_attr_accessor :id, :address
+
+  has_many :humans,
+    class_name: 'Humans',
+    foreign_key: :house_id,
+    primary_key: :id
+end
+
+puts 'simply find queries:'
+puts '-------------------'
+bike = Bike.find(2)
+puts "bike = Bike.find(2)       => #{bike.inspect}"
+puts "bike.name                => #{bike.name}"
+
+puts
+
+human = Human.find(1)
+puts "human = Human.find(1)   => #{human.inspect}"
+puts "human.fname             => #{human.fname}"
+
+puts
+
+puts 'belongs_to associations:'
+puts '-----------------------'
+puts "bike.human               => #{bike.human.inspect}"
+puts "bike.human.fname:        => #{bike.human.fname}"
+puts "human.house.address:    => #{human.house.address}"
+
+puts
+
+puts 'has_many associations:'
+puts '---------------------'
+puts "human.bikes              => #{human.bikes.inspect}"
+
+puts
+
+puts 'has_one_through associations:'
+puts '----------------------------'
+puts "bike.house               => #{bike.house.inspect}"
+puts "bike.house.address:      => #{bike.house.address}"

4cc0b4fecef01c7c4767b827815fb86bb67adbde
Megan Anderson <megroseanderson@gmail.com>
2016-05-19 18:54:26 -0700

    delete unnecessary files

--- active_record_lite.rb
+++
@@ -1,7 +0,0 @@
-require 'active_support/inflector'
-require 'db_connection'
-require_relative '00_attr_accessor_object'
-require_relative '01_sql_object'
-require_relative '02_searchable'
-require_relative '03_associatable'
-require_relative '04_associatable2'
